68047e2be54dd94688df94ad23d34b411af4cb86
derekgygax <derekgygax@gmail.com>
2024-09-29 16:24:16 -0400

    Initial commit for blogservice project

---
+++ src/main/java/com/vva/blogservice/BlogserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vva.blogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogserviceApplication.class, args);
+	}
+
+}
